84fc3f204eb9865ab2181918b6ee22aba458632d
Anders Ramsay <andersr@gmail.com>
2016-02-18 16:38:06 -0500

    removed unneeded mock component

--- app/tests/jasmine/client/integration/TestSpec.js
+++ app/tests/jasmine/client/integration/TestSpec.js
@@ -8,20 +8,15 @@ describe('SingleFieldSubmit Component', function () {
     let component = TestUtils.renderIntoDocument(
     	  React.createElement(SingleFieldSubmit, {handleInput: callbackSpy})
     	  );
-    // let node     = ReactDOM.findDOMNode(parentComponent);
-    // let $node    = $(node);
     let form = TestUtils.findRenderedDOMComponentWithTag(component, 'form');
     let input = TestUtils.findRenderedDOMComponentWithTag(component, 'input');
-    // expect(parentComponent.state.callbackValue).toBe(null);
+
 
     //TEST
     Simulate.change(input, { target: { value: mockUserInput } });
     Simulate.submit(form);
     expect(callbackSpy).toHaveBeenCalledWith(mockUserInput);
     expect(callbackSpy.calls.count()).toEqual(1);
-    // Simulate.change($node.find('input')[0], { target: { value: mockUserInput } });
-    // Simulate.submit($node.find('form')[0]);
-    // expect(parentComponent.state.callbackValue).toBe(mockUserInput);
   });
 
 });
