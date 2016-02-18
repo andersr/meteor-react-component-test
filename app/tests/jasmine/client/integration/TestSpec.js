810b6baeca4328830b46059f702edf964b300781
Anders Ramsay <andersr@gmail.com>
2016-02-18 16:36:47 -0500

    refactored using spies

--- app/tests/jasmine/client/integration/TestSpec.js
+++ app/tests/jasmine/client/integration/TestSpec.js
@@ -3,18 +3,25 @@ describe('SingleFieldSubmit Component', function () {
   it('should, on submit, return the value input into the form', function () {
     
     //SETUP
+    let callbackSpy = jasmine.createSpy('callbackSpy');
     let mockUserInput = 'Test input';
-    let parentComponent = TestUtils.renderIntoDocument(
-    	  React.createElement(MockParentComponent)
+    let component = TestUtils.renderIntoDocument(
+    	  React.createElement(SingleFieldSubmit, {handleInput: callbackSpy})
     	  );
-    let node     = ReactDOM.findDOMNode(parentComponent);
-    let $node    = $(node);
-    expect(parentComponent.state.callbackValue).toBe(null);
+    // let node     = ReactDOM.findDOMNode(parentComponent);
+    // let $node    = $(node);
+    let form = TestUtils.findRenderedDOMComponentWithTag(component, 'form');
+    let input = TestUtils.findRenderedDOMComponentWithTag(component, 'input');
+    // expect(parentComponent.state.callbackValue).toBe(null);
 
     //TEST
-    Simulate.change($node.find('input')[0], { target: { value: mockUserInput } });
-    Simulate.submit($node.find('form')[0]);
-    expect(parentComponent.state.callbackValue).toBe(mockUserInput);
+    Simulate.change(input, { target: { value: mockUserInput } });
+    Simulate.submit(form);
+    expect(callbackSpy).toHaveBeenCalledWith(mockUserInput);
+    expect(callbackSpy.calls.count()).toEqual(1);
+    // Simulate.change($node.find('input')[0], { target: { value: mockUserInput } });
+    // Simulate.submit($node.find('form')[0]);
+    // expect(parentComponent.state.callbackValue).toBe(mockUserInput);
   });
 
 });
--- app/tests/jasmine/client/integration/mockComponents/reactMockComponents.jsx
+++ app/tests/jasmine/client/integration/mockComponents/reactMockComponents.jsx
@@ -1,22 +1,17 @@
-MockParentComponent = React.createClass({
-	 getDefaultProps() {
-    return {
-      componentBeingTested: null
-    };
-  },
-  getInitialState: function() {
-    return {
-      callbackValue: null
-    };
-  },
-  handleCallback: function(value) {
-    this.setState({callbackValue: value});
-  },
-  render: function() {
-    return (
-      <div className="container">
-        <SingleFieldSubmit handleInput={this.handleCallback} />
-      </div>
-    )
-  }
-});
+// MockParentComponent = React.createClass({
+//   getInitialState: function() {
+//     return {
+//       callbackValue: null
+//     };
+//   },
+//   handleCallback: function(value) {
+//     this.setState({callbackValue: value});
+//   },
+//   render: function() {
+//     return (
+//       <div className="container">
+//         <SingleFieldSubmit handleInput={this.handleCallback} />
+//       </div>
+//     )
+//   }
+// });
